ce9df7a210282e0d075713a8aadd2ee738d3f387
chandang07 <chandan.g@qualitestgroup.com>
2023-05-02 11:08:52 +0530

    commit

--- src/test/java/Sample/demo.java
+++ src/test/java/Sample/demo.java
@@ -1,6 +1,13 @@
 package Sample;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class demo {
+    @Test
     public void sampleTest(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
