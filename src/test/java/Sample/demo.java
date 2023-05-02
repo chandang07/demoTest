421a02f0e3d52ee0a4cb34bcfdac4454f81f3ad4
chandang07 <chandan.g@qualitestgroup.com>
2023-05-02 11:01:00 +0530

    commit

--- src/test/java/Sample/demo.java
+++ src/test/java/Sample/demo.java
@@ -1,7 +1,21 @@
 package Sample;
 
 public class demo {
-    public static void main(String[] args) {
+    public void sampleTest(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
+        driver.get("https://opensource-demo.orangehrmlive.com");
+
+       // Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+       // Thread.sleep(2000);
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+
+        driver.close();
     }
 }
