bf7d44e2442f15da8d33042a526843f932d32906
chandang07 <chandan.g@qualitestgroup.com>
2023-05-04 14:01:03 +0530

    commit

--- src/test/java/Sample/demo.java
+++ src/test/java/Sample/demo.java
@@ -8,19 +8,19 @@ import org.testng.annotations.Test;
 
 public class demo {
     @Test
-    public void sampleTest(String[] args) {
+    public void sampleTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://opensource-demo.orangehrmlive.com");
 
-       // Thread.sleep(2000);
+        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-       // Thread.sleep(2000);
+        Thread.sleep(2000);
         Assert.assertEquals(driver.getTitle(), "OrangeHRM");
 
         driver.close();
